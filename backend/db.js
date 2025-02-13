4f2403cbf4d5c65e6ed78479f9c170c451551fc3
Shryumeghre <2021bcs082@sggs.ac.in>
2025-02-13 11:56:33 +0530

    Made some changes in UI

--- backend/db.js
+++ backend/db.js
@@ -10,9 +10,9 @@ const db = mysql.createConnection({
 
 db.connect(err => {
     if (err) {
-        console.error("❌ Database connection failed: " + err.message);
+        console.error("Database connection failed: " + err.message);
     } else {
-        console.log("✅ Connected to MySQL Database!");
+        console.log("Connected to MySQL Database!");
     }
 });
 
